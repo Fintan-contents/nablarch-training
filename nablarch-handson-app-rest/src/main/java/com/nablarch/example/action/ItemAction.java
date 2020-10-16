e23adf86edcee2649f8994b98460094664aa5ffa
developer-mkh <mkh@lares.dti.ne.jp>
2020-10-16 16:28:19 +0900

    インポート文の再編成

--- nablarch-handson-app-rest/src/main/java/com/nablarch/example/action/ItemAction.java
+++ nablarch-handson-app-rest/src/main/java/com/nablarch/example/action/ItemAction.java
@@ -12,7 +12,12 @@ import nablarch.fw.web.HttpRequest;
 import nablarch.fw.web.HttpResponse;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
