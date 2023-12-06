8e8f64131dec15e45e13dbca0ae805d63ec9174b
Habu Miki <developer.mkh@gmail.com>
2023-12-06 18:50:18 +0900

    5u23対応 Nablarch j-batchアプリ完成例

--- nablarch-handson-app-batch-ee/src/main/java/com/nablarch/example/app/batch/ee/chunk/EmployeeSearchReader.java
+++ nablarch-handson-app-batch-ee/src/main/java/com/nablarch/example/app/batch/ee/chunk/EmployeeSearchReader.java
@@ -1,14 +1,18 @@
 package com.nablarch.example.app.batch.ee.chunk;
 
-import com.nablarch.example.app.batch.ee.form.EmployeeForm;
+import java.io.Serializable;
+import java.util.Iterator;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import nablarch.common.dao.DeferredEntityList;
 import nablarch.common.dao.UniversalDao;
 import nablarch.fw.batch.ee.chunk.BaseDatabaseItemReader;
+import nablarch.fw.batch.ee.progress.ProgressManager;
 
-import javax.enterprise.context.Dependent;
-import javax.inject.Named;
-import java.io.Serializable;
-import java.util.Iterator;
+import com.nablarch.example.app.batch.ee.form.EmployeeForm;
 
 /**
  * 社員情報をDBから取得する{@link javax.batch.api.chunk.ItemReader}実装クラス。
@@ -25,8 +29,24 @@ public class EmployeeSearchReader extends BaseDatabaseItemReader {
     /** 社員情報を保持するイテレータ */
     private Iterator<EmployeeForm> iterator;
 
+    /** 進捗管理Bean */
+    private final ProgressManager progressManager;
+
+    /**
+     * コンストラクタ。
+     *
+     * @param progressManager 進捗管理Bean
+     */
+    @Inject
+    public EmployeeSearchReader(ProgressManager progressManager) {
+        this.progressManager = progressManager;
+    }
+
     @Override
     public void doOpen(Serializable checkpoint) throws Exception {
+
+        progressManager.setInputCount(UniversalDao.countBySqlFile(EmployeeForm.class, "SELECT_EMPLOYEE"));
+
         list = (DeferredEntityList<EmployeeForm>) UniversalDao.defer()
                 .findAllBySqlFile(EmployeeForm.class, "SELECT_EMPLOYEE");
         iterator = list.iterator();
