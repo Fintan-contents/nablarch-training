f0bf6435dcec970c92f9ff11eac167e0f250fb75
Habu Miki <developer.mkh@gmail.com>
2023-12-14 11:12:38 +0900

    5u23対応 handson-02

--- handson-02/src/main/java/com/nablarch/example/app/web/action/AuthenticationAction.java
+++ handson-02/src/main/java/com/nablarch/example/app/web/action/AuthenticationAction.java
@@ -1,11 +1,6 @@
 package com.nablarch.example.app.web.action;
 
-import com.nablarch.example.app.entity.SystemAccount;
-import com.nablarch.example.app.entity.Users;
-import com.nablarch.example.app.web.common.authentication.AuthenticationUtil;
-import com.nablarch.example.app.web.common.authentication.context.LoginUserPrincipal;
-import com.nablarch.example.app.web.common.authentication.exception.AuthenticationException;
-import com.nablarch.example.app.web.form.LoginForm;
+import nablarch.common.authorization.role.session.SessionStoreUserRoleUtil;
 import nablarch.common.dao.UniversalDao;
 import nablarch.common.web.csrf.CsrfTokenUtil;
 import nablarch.common.web.session.SessionUtil;
@@ -18,6 +13,15 @@ import nablarch.fw.ExecutionContext;
 import nablarch.fw.web.HttpRequest;
 import nablarch.fw.web.HttpResponse;
 import nablarch.fw.web.interceptor.OnError;
+
+import com.nablarch.example.app.entity.SystemAccount;
+import com.nablarch.example.app.entity.Users;
+import com.nablarch.example.app.web.common.authentication.AuthenticationUtil;
+import com.nablarch.example.app.web.common.authentication.context.LoginUserPrincipal;
+import com.nablarch.example.app.web.common.authentication.exception.AuthenticationException;
+import com.nablarch.example.app.web.form.LoginForm;
+
+import java.util.Collections;
 
 /**
  * 認証アクション。
@@ -74,7 +78,13 @@ public class AuthenticationAction {
         CsrfTokenUtil.regenerateCsrfToken(context);
 
         LoginUserPrincipal userContext = createLoginUserContext(form.getLoginId());
-        SessionUtil.put(context, "userContext", userContext, "httpSession");
+
+        if (userContext.isAdmin()) {
+            SessionStoreUserRoleUtil.save(Collections.singleton(LoginUserPrincipal.ROLE_ADMIN), context);
+        }
+
+        SessionUtil.put(context, "userContext", userContext);
+        SessionUtil.put(context,"user.id",String.valueOf(userContext.getUserId()));
         return new HttpResponse(303, "redirect:///action/project/index");
     }
 
@@ -93,6 +103,7 @@ public class AuthenticationAction {
         LoginUserPrincipal userContext = new LoginUserPrincipal();
         userContext.setUserId(account.getUserId());
         userContext.setKanjiName(users.getKanjiName());
+        userContext.setAdmin(account.isAdminFlag());
         userContext.setLastLoginDateTime(account.getLastLoginDateTime());
 
         return userContext;
